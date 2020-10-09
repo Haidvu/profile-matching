97a116cf2de8b4889d82d4b00ce360a347a529fa
Muhammad Usman <muhammadusman0200@gmail.com>
2020-10-09 17:28:40 -0500

    template

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
-import Navbar from "./components/Navbar/Navbar";
+import Login from "./components/Login/Login"
+import Signup from "./components/Signup/Signup"
+import StudentInfo from "./components/AccountInfo/StudentInfo"
+import CompanyInfo from "./components/AccountInfo/CompanyInfo"
 
 const App = () => {
   return (
     <Router>
-      <Navbar />
       <Route path="/" exact component={Home} />
+      <Route path="/login" component={Login} />
+      <Route path="/signup"  component={Signup} />
+      <Route path="/studentinfo" component={StudentInfo} />
+      <Route path="/companyinfo" component={CompanyInfo} />
     </Router>
   );
 };
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,10 @@
 import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
 
 const Home = () => {
   return (
     <div>
-      <h1>Test</h1>
-      <h1>Test</h1>
+      <Navbar />
     </div>
   );
 };
