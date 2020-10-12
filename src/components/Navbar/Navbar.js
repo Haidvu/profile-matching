c41de97e4f9e7037f81b8b1b580b362a82962ca7
mayoor shardha <mayoor.shardha@hp.com>
2020-10-11 23:12:22 -0500

    Mayoor Updates with HomePage And NavBar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,24 +1,55 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, IconButton } from "@material-ui/core";
-import MenuIcon from "@material-ui/icons/Menu";
+import { AppBar, Toolbar, Icon, Button, Grid } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+import UHLogo from "../../assets/UHLogo.png";
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
-const Navbar = (props) => {
+const useStyles = makeStyles((theme) => ({
+  logo:{
+    objectFit:"contain",
+    width:"5%",
+    float:"left",
+    height:"5%"
+  },
+  AppBar:{
+    background:"rgba(200,16,46,1)",
+    display:"flex",
+    width:"98%",
+    borderRadius:"3%",
+    margin:"10px 10px 10px 10px"
+  },
+  Login:{
+    objectFit:"contain",
+  },
+  SignUp:{
+    objectFit:"contain",
+  },
+}));
+
+export default function Navbar() {
+  const classes = useStyles();
+
   return (
     <div>
-      <div>
-        <AppBar position="static">
-          <Toolbar variant="dense">
-            <IconButton edge="start" color="inherit" aria-label="menu">
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" color="inherit">
-              Sample
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </div>
+      <React.Fragment>
+      <AppBar className={classes.AppBar} position="fixed">
+        <Toolbar>
+          <Grid item>
+            <img className={classes.logo} src={UHLogo}/>
+          </Grid>
+          <Grid item>
+            <AccountCircleIcon/>
+          </Grid>
+          <Grid item> 
+            <Button size="small" color="inherit" className={classes.Login}>Login</Button>
+          </Grid>
+          <Grid item> 
+            <Button size="small" color="inherit" className={classes.SignUp}>Sign Up</Button>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+      </React.Fragment>
     </div>
   );
-};
+}
 
-export default Navbar;
