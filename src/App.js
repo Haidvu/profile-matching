7974c2aaaf96032ce2da2122eb2b298c836609d7
Muhammad Usman <muhammadusman0200@gmail.com>
2020-10-14 11:30:27 -0500

    404 and fixed some routing stuff

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./components/Login/Login";
 import Signup from "./components/Signup/Signup";
 import StudentInfo from "./components/AccountInfo/StudentInfo";
 import CompanyInfo from "./components/AccountInfo/CompanyInfo";
-import Dashboard from "./pages/Dashboard/Dashboard";
 import ProtectedRoute from "./components/ProtecteRoute/ProtecteRoute";
 import Profile from "./pages/Profile/Profile";
 import Projects from "./pages/Projects/Projects";
 import DataContextProvider from "./contexts/dataContext";
+import ProtectedDashboard from "./components/Dashboard/Dashboard";
+import NotFound from "./pages/NotFound/NotFound";
 
 const App = () => {
   return (
     <Router>
-      <Route path="/" exact component={Home} />
-      <Route path="/login" component={Login} />
-      <Route path="/signup" component={Signup} />
-      <Route path="/studentinfo" component={StudentInfo} />
-      <Route path="/companyinfo" component={CompanyInfo} />
-      <ProtectedRoute path="/dashboard" component={Dashboard} />
-      <DataContextProvider>
-        <ProtectedRoute path="/dashboard/profile" component={Profile} />
-        <ProtectedRoute path="/dashboard/projects" component={Projects} />
-      </DataContextProvider>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/studentinfo" component={StudentInfo} />
+        <Route path="/companyinfo" component={CompanyInfo} />
+        <DataContextProvider>
+          <ProtectedRoute path="/profile" component={Profile} />
+          <ProtectedRoute path="/projects" component={Projects} />
+        </DataContextProvider>
+        <Route path="*" component={NotFound} />
+      </Switch>
+      <ProtectedDashboard />
     </Router>
   );
 };
---
+++ src/pages/NotFound/NotFound.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>404</h1>
+      <h6>Page Not Found</h6>
+      <Button>Go back to Home</Button>
+    </div>
+  );
+};
+
+export default NotFound;
