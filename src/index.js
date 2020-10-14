d8bab02257ea6aabcde56de4de6af6e3932b368d
Muhammad Usman <muhammadusman0200@gmail.com>
2020-10-14 12:03:34 -0500

    edited 404

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import DataContextProvider from "./contexts/dataContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DataContextProvider>
+      <App />
+    </DataContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
