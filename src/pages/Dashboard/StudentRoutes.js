fe2aba355c927f2e761a23ca3fd8600a970bfcdc
Muhammad Usman <mohammadusmanxero@gmail.com>
2020-12-19 12:52:18 -0600

    template for student search

--- src/pages/Dashboard/StudentRoutes.js
+++ src/pages/Dashboard/StudentRoutes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, useRouteMatch, Switch } from "react-router-dom";
 import StudentProfile from "../StudentProfile/StudentProfile";
+import { StudentSearch } from "../StudentSearch/StudentSearch";
 
 const StudentRoutes = () => {
   let { path } = useRouteMatch();
@@ -8,6 +9,7 @@ const StudentRoutes = () => {
     <div>
       <Switch>
         <Route path={path} exact component={StudentProfile} />
+        <Route path={`${path}/search`} exact component={StudentSearch} />
         {/* <Route
           path={`${path}/*`}
           render={() => (
