8e506bb30e055ad4c83a375257a208a393a30588
sdpa <sdpa14@gmail.com>
2021-01-09 11:45:33 -0600

    refactor for authentication

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,26 +1,7 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Route, Redirect } from "react-router-dom";
-import { getNewToken } from "../Login/Login";
-import axios from "axios";
 
 const ProtectedRoute = ({ component: Component, ...rest }) => {
-  //If login fails -> obtain new token, if that fails -> log the uer out.
-  useEffect(() => {
-    let token = localStorage.getItem("token");
-    if (token) {
-      axios
-        .post("http://18.213.74.196:8000/api/token/refresh/", {
-          refresh: localStorage.getItem("refresh"),
-        })
-        .then((res) => {
-          localStorage.setItem("token", res.data.access);
-          setTimeout(getNewToken, 17900 * 1000); //Get new token approxiamtey every 4 hrs and 58 min.
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-  }, []);
   return (
     <Route
       {...rest}
